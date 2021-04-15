623a20a66ed7dd9126ee5c185b627b7ac26b9b8e
v1itor <49135561+v1itor@users.noreply.github.com>
2021-04-15 13:54:19 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDse9h6fbMHH0qrPgEuLusQf2VFljGlLfw",
+    authDomain: "tdsobresuaviagem.firebaseapp.com",
+    databaseURL: "https://tdsobresuaviagem.firebaseio.com",
+    projectId: "tdsobresuaviagem",
+    storageBucket: "tdsobresuaviagem.appspot.com",
+    messagingSenderId: "163821828986",
+    appId: "1:163821828986:web:f89c583661d44e84e9b4c3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
